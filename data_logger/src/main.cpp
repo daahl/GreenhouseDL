549f8c333bde2284424d295f91e26659f595392d
Mac D <ma.dah@outlook.com>
2022-03-12 16:20:41 +0100

    Tidy up code and set sleep to 20 min

--- data_logger/src/main.cpp
+++ data_logger/src/main.cpp
@@ -1,121 +1,142 @@
 /*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-mysql-database-php/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
+    Data logger for my greenhouse. Gotta know how those tomatoes are doing!
+    Big thanks to https://randomnerdtutorials.com for all the amazing guides.
+    This project would have been a lot less smooth without them.
 
+    Feel free to reuse all or any of this code. Beerware, or something like that.
+
+    By Marcus Dahlström 2022.
 */
+#include <Arduino.h>
+#include <esp_adc_cal.h>
+// WiFi library
+#include <WiFi.h>
+// HTTP required to send push request to server
+#include <HTTPClient.h>
+// Temp and humidity sensor libraries
+#include <SPI.h>
+#include <Adafruit_Si7021.h>
+// Local file not pushed to git with passwords, ssid, etc.
+#include "password_config.h"
 
-#ifdef ESP32
-  #include <WiFi.h>
-  #include <HTTPClient.h>
-#else
-  #include <ESP8266WiFi.h>
-  #include <ESP8266HTTPClient.h>
-  #include <WiFiClient.h>
-#endif
+// Number of seconds between sensor readings = deep sleep time
+const int SLEEPTIME = 1200;
 
-#include <Wire.h>
+// Data to be sent with the push request to the server
+const String TEMPHUMSENSOR = "Si7021";
+const String LOCATION = "Front greenhouse";
+float currentTemp;
+float currentHum;
 
-// Replace with your network credentials
-const char* ssid     = "removedforgit";
-const char* password = "removedforgit";
-
-// REPLACE with your Domain name and URL path or IP address with path
-const char* serverName = "http://192.168.1.100:8080/greenhouse/post_esp_data.php";
-
-// Keep this API Key value to be compatible with the PHP code provided in the project page. 
-// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key 
-String apiKeyValue = "tPmAT5Ab3j7F9";
-
-String sensorName = "BME280";
-String sensorLocation = "Office";
-
-/*#include <SPI.h>
-#define BME_SCK 18
-#define BME_MISO 19
-#define BME_MOSI 23
-#define BME_CS 5*/
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-//Adafruit_BME280 bme;  // I2C
-//Adafruit_BME280 bme(BME_CS);  // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);  // software SPI
-
-void setup() {
-  Serial.begin(9600);
-  
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) { 
-    delay(500);
-    Serial.print(".");
-  }
+/*
+    Helper functions to keep the main program tidier
+*/
+void disableWiFi(){
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
   Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
-
-  // (you can also pass in a Wire library object like &Wire2)
-  //bool status = bme.begin(0x76);
-  /*if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring or change I2C address!");
-    while (1);
-  }*/
+  Serial.println("WiFi disconnected");
 }
 
-void loop() {
-  //Check WiFi connection status
-  if(WiFi.status()== WL_CONNECTED){
-    WiFiClient client;
-    HTTPClient http;
-    
-    // Your Domain name with URL path or IP address with path
-    http.begin(client, serverName);
-    
-    // Specify content-type header
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    
-    // Prepare your HTTP POST request data
-    /*String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
-                          + "&location=" + sensorLocation + "&value1=" + String(bme.readTemperature())
-                          + "&value2=" + String(bme.readHumidity()) + "&value3=" + String(bme.readPressure()/100.0F) + "";
-    Serial.print("httpRequestData: ");
-    Serial.println(httpRequestData);*/
-    
-    // You can comment the httpRequestData variable above
-    // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
-    String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&location=Office&value1=24.75&value2=49.54&value3=1005.14";
+void enableWiFi(){
+  adc_power_on();
+  WiFi.disconnect(false);
+  WiFi.mode(WIFI_STA);
+ 
+  Serial.println("WiFi connecting...");
+  // Credentials saved in local file
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+ 
+  // Wait for the wifi connection
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
-    // Send HTTP POST request
-    int httpResponseCode = http.POST(httpRequestData);
-     
-    // If you need an HTTP request with a content type: text/plain
-    //http.addHeader("Content-Type", "text/plain");
-    //int httpResponseCode = http.POST("Hello, World!");
-    
-    // If you need an HTTP request with a content type: application/json, use the following:
-    //http.addHeader("Content-Type", "application/json");
-    //int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
-        
-    if (httpResponseCode>0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-    }
-    else {
-      Serial.print("Error code: ");
-      Serial.println(httpResponseCode);
-    }
-    // Free resources
-    http.end();
+void getthSensorReadings(){
+  // Temp sensor object
+  Adafruit_Si7021 thSensor = Adafruit_Si7021();
+
+  // Open the sensor connection
+  Serial.println("Connecting to sensor...");
+  while(!thSensor.begin()){
+    delay(100);
+  }
+
+  // Turn off heater, as we dont need it for short measurements
+  thSensor.heater(false);
+
+  currentTemp = thSensor.readTemperature();
+  currentHum = thSensor.readHumidity();
+
+  Serial.println(currentTemp);
+  Serial.println(currentHum);
+}
+
+void sendDataToServer(){
+  WiFiClient client;
+  HTTPClient http;
+
+  // Connect to the server. Credentials saved in local file
+  http.begin(client, LAMP_SERVER);
+
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  // Prepare your HTTP POST request data
+  String httpRequestData = "api_key=" + String(API_KEY) + "&sensor=" + TEMPHUMSENSOR
+                          + "&location=" + LOCATION + "&temperature=" + String(currentTemp)
+                          + "&humidity=" + String(currentHum);
+  Serial.print("httpRequestData: ");
+  Serial.println(httpRequestData);
+
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(httpRequestData);
+
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
   }
   else {
-    Serial.println("WiFi Disconnected");
+     Serial.print("Error code: ");
+     Serial.println(httpResponseCode);
   }
-  //Send an HTTP POST request every 30 seconds
-  delay(30000);  
+  // Free resources
+  http.end();
 }
+
+/*
+    This is the main program, as deep sleep prevents the loop() from running.
+*/
+void setup(){
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Data logger started");
+  adc_power_on();
+
+  // Connect to wifi before and start logging
+  enableWiFi();
+  getthSensorReadings();
+  sendDataToServer();
+
+  // Enter deep sleep
+  Serial.println("");
+  Serial.printf("Going to sleep for %d seconds", SLEEPTIME);
+  // Disconnect everything before going to sleep
+  disableWiFi();
+  btStop();
+  adc_power_off();
+
+  esp_sleep_enable_timer_wakeup(SLEEPTIME * 1000 * 1000);
+  esp_deep_sleep_start();
+}
+
+void loop(){
+}
+
